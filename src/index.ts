6804d177151f3c395c1316b046fc18be92aeb200
Carlos Borrero <53616525+Sonrak141@users.noreply.github.com>
2023-11-13 00:31:50 -0500

    Deploy Api

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,5 @@ app.use(cors()) // Use this after the variable declaration
 
 app.use('/api', gamesRoutes);
 
-app.listen(3000)
-console.log("Server port", 3000)
+app.listen(3001)
+console.log("Server port", 3001)
